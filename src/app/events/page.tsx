18b2d06b214516be59e1708d7db8987892e00b37
ojela028xamk <jerelans91@gmail.com>
2024-08-05 10:59:08 +0300

    events component

---
+++ src/app/events/page.tsx
@@ -0,0 +1,11 @@
+import css from "./events.module.scss";
+
+const Events = () => {
+  return (
+    <div className={css.events}>
+      <h1>Events</h1>
+    </div>
+  );
+};
+
+export default Events;
