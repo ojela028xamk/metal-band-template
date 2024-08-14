20fa5bf049cb8ff19a0fe17fc6013733360d27a3
ojela028xamk <jerelans91@gmail.com>
2024-08-14 13:53:16 +0300

    photos component

---
+++ src/app/photos/page.tsx
@@ -0,0 +1,11 @@
+import css from "./photos.module.scss";
+
+const Photos = () => {
+  return (
+    <div className={css.photos}>
+      <h1>Photos</h1>
+    </div>
+  );
+};
+
+export default Photos;
