7b4d1ff84881a5526d003f53c35005d98e6a44a5
ojela028xamk <jerelans91@gmail.com>
2024-08-14 14:07:47 +0300

    photos list

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -1,9 +1,21 @@
+"use client";
+import Image from "next/image";
 import css from "./photos.module.scss";
+import { bandPhotos } from "./photosList";
 
 const Photos = () => {
   return (
     <div className={css.photos}>
       <h1>Photos</h1>
+      {bandPhotos.map((photo) => (
+        <Image
+          className={css.band_photo}
+          src={photo.url}
+          alt={photo.author}
+          width={200}
+          height={200}
+        />
+      ))}
     </div>
   );
 };
---
+++ src/app/photos/photosList.ts
@@ -0,0 +1,43 @@
+type Photo = {
+  author: string;
+  url: string;
+};
+
+export const bandPhotos: Photo[] = [
+  {
+    author: "Photo by Microsoft Designer",
+    url: "/band_photo_1.jpg",
+  },
+  {
+    author: "Photo by Microsoft Designer",
+    url: "/band_photo_2.jpg",
+  },
+  {
+    author: "Photo by Microsoft Designer",
+    url: "/band_photo_3.jpeg",
+  },
+  {
+    author: "Photo by Microsoft Designer",
+    url: "/band_photo_4.jpeg",
+  },
+  {
+    author: "Photo by Microsoft Designer",
+    url: "/band_photo_5.jpeg",
+  },
+  {
+    author: "Photo by David Bartus",
+    url: "/band_photo_6.jpg",
+  },
+  {
+    author: "Photo by Microsoft Designer",
+    url: "/band_photo_7.jpeg",
+  },
+  {
+    author: "Photo by Vishnu R Nair",
+    url: "/band_photo_8.jpg",
+  },
+  {
+    author: "Photo by Wendy Wei",
+    url: "/band_photo_9.jpg",
+  },
+];
