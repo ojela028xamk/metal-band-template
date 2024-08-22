1d583fa735498f4cc430e002dbdb0cb0c195047a
ojela028xamk <jerelans91@gmail.com>
2024-08-22 12:42:50 +0300

    navcard with image and text

--- src/app/Navigation/NavigationCard.tsx
+++ src/app/Navigation/NavigationCard.tsx
@@ -1,15 +1,27 @@
 import Link from "next/link";
 import { NavigationLinks } from "./Navigation";
 import css from "./NavigationCard.module.scss";
+import Image from "next/image";
 
 type NavigationCardProps = {
   link: NavigationLinks;
 };
 
 const NavigationCard = ({ link }: NavigationCardProps) => {
+  const linkName = link.charAt(0).toUpperCase() + link.slice(1);
+
   return (
     <div className={css.navigation_card}>
-      <Link href={link}>{link}</Link>
+      <Image
+        className={css.card_image}
+        src={"/card_images/band_card.jpg"}
+        alt={"Card"}
+        width="200"
+        height="200"
+      />
+      <Link className={css.card_link} href={link}>
+        {linkName}
+      </Link>
     </div>
   );
 };
