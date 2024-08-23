79a1ab398a475cb387e6e9878c1c67c904ff8373
ojela028xamk <jerelans91@gmail.com>
2024-08-23 14:41:35 +0300

    nav to center on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 import css from "./page.module.scss";
 
 const Home = () => {
-  return (
-    <div className={css.main}>
-      <h1>This is a homepage!</h1>
-    </div>
-  );
+  return <div className={css.main}></div>;
 };
 
 export default Home;
