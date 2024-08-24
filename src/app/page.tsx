0c55ecf29bc3f8f945c454ed3e0d42d96a592605
ojela028xamk <jerelans91@gmail.com>
2024-08-24 11:59:12 +0300

    video background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 import css from "./page.module.scss";
 
 const Home = () => {
-  return <div className={css.main}></div>;
+  return (
+    <div className={css.main}>
+      <video className={css.video} loop autoPlay muted>
+        <source src="/background.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
 };
 
 export default Home;
