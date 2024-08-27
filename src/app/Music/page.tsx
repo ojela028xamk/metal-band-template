c7bae213e9279f7b5cea058953d6aa54204f445a
ojela028xamk <jerelans91@gmail.com>
2024-08-27 12:35:07 +0300

    album css structure

--- src/app/Music/page.tsx
+++ src/app/Music/page.tsx
@@ -7,22 +7,28 @@ const Music = () => {
     <div className={css.music}>
       <h1>Music</h1>
       {bandAlbums.map((album) => (
-        <div key={album.album_name}>
-          <h3>{album.album_name}</h3>
-          <p>
-            {album.year} - {album.type}
-          </p>
-          <ol>
-            {album.tracks.map((track) => (
-              <li key={track}>{track}</li>
-            ))}
-          </ol>
-          <Image
-            src={album.url}
-            alt={album.album_name}
-            height={200}
-            width={200}
-          />
+        <div className={css.album} key={album.album_name}>
+          <div className={css.album_image}>
+            <Image
+              className={css.image}
+              src={album.url}
+              alt={album.album_name}
+              height={0}
+              width={0}
+              layout="responsive"
+            />
+          </div>
+          <div className={css.album_info}>
+            <h3 className={css.album_header}>{album.album_name}</h3>
+            <p className={css.album_paragraph}>
+              {album.year} - {album.type}
+            </p>
+            <ol className={css.album_tracks}>
+              {album.tracks.map((track) => (
+                <li key={track}>{track}</li>
+              ))}
+            </ol>
+          </div>
         </div>
       ))}
     </div>
