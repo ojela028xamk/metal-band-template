dc1cee2a244d02ea8a7b217771f10a5e340f2399
ojela028xamk <jerelans91@gmail.com>
2024-08-29 15:54:55 +0300

    better names

--- src/app/Music/page.tsx
+++ src/app/Music/page.tsx
@@ -5,7 +5,6 @@ import css from "./music.module.scss";
 const Music = () => {
   return (
     <div className={css.music}>
-      <h1>Music</h1>
       {bandAlbums.map((album) => (
         <div className={css.album} key={album.album_name}>
           <div className={css.album_image}>
@@ -19,10 +18,9 @@ const Music = () => {
             />
           </div>
           <div className={css.album_info}>
-            <h3 className={css.album_header}>{album.album_name}</h3>
-            <p className={css.album_paragraph}>
-              {album.year} - {album.type}
-            </p>
+            <h3 className={css.album_name}>{album.album_name}</h3>
+            <p className={css.album_year}>{album.year}</p>
+            <p className={css.album_type}>{album.type}</p>
             <ol className={css.album_tracks}>
               {album.tracks.map((track) => (
                 <li key={track}>{track}</li>
