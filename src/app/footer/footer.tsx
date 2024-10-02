e6fca90f71635b1b64f221766f7afda661ae6fbb
ojela028xamk <jerelans91@gmail.com>
2024-10-02 12:23:03 +0300

    footer component

---
+++ src/app/footer/footer.tsx
@@ -0,0 +1,11 @@
+import css from "./footer.module.scss";
+
+const Footer = () => {
+  return (
+    <div className={css.footer}>
+      <span>Powered by: Microsoft Designer, DeepAI Text Generator</span>
+    </div>
+  );
+};
+
+export default Footer;
