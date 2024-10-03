830d084fdc6792d1bae6f4838ee3b5e05d939ca8
ojela028xamk <jerelans91@gmail.com>
2024-10-03 13:11:25 +0300

    footer design and global margin

--- src/app/footer/footer.tsx
+++ src/app/footer/footer.tsx
@@ -1,9 +1,26 @@
+import Image from "next/image";
 import css from "./footer.module.scss";
 
 const Footer = () => {
   return (
     <div className={css.footer}>
-      <span>Powered by: Microsoft Designer, DeepAI Text Generator</span>
+      <span className={css.footer_text}>Powered by: </span>
+      <Image
+        className={css.footer_image}
+        src={"/ms_designer_logo.png"}
+        alt={"Microsoft Designer Logo"}
+        width={40}
+        height={40}
+      />
+      <span className={css.footer_text}>Microsoft Designer | </span>
+      <Image
+        className={css.footer_image}
+        src={"/deepai_logo.png"}
+        alt={"DeepAI Logo"}
+        width={40}
+        height={40}
+      />
+      <span className={css.footer_text}>DeepAI Text Generator</span>
     </div>
   );
 };
