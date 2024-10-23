8825aee1ad62e00c054bff02bfbbf73aeb5b67c4
ojela028xamk <jerelans91@gmail.com>
2024-10-23 12:32:52 +0300

    band photo renames

--- src/app/photos/photosList.ts
+++ src/app/photos/photosList.ts
@@ -6,38 +6,38 @@ type Photo = {
 export const bandPhotos: Photo[] = [
   {
     author: "Photo by Microsoft Designer",
-    url: "/band_photo_1.jpg",
+    url: "/band_images/band_photo_1.jpg",
   },
   {
     author: "Photo by Microsoft Designer",
-    url: "/band_photo_2.jpg",
+    url: "/band_images/band_photo_2.jpg",
   },
   {
     author: "Photo by Microsoft Designer",
-    url: "/band_photo_3.jpeg",
+    url: "/band_images/band_photo_3.jpeg",
   },
   {
     author: "Photo by Microsoft Designer",
-    url: "/band_photo_4.jpeg",
+    url: "/band_images/band_photo_4.jpeg",
   },
   {
     author: "Photo by Microsoft Designer",
-    url: "/band_photo_5.jpeg",
+    url: "/band_images/band_photo_5.jpeg",
   },
   {
     author: "Photo by David Bartus",
-    url: "/band_photo_6.jpg",
+    url: "/band_images/band_photo_6.jpg",
   },
   {
     author: "Photo by Microsoft Designer",
-    url: "/band_photo_7.jpeg",
+    url: "/band_images/band_photo_7.jpeg",
   },
   {
     author: "Photo by Vishnu R Nair",
-    url: "/band_photo_8.jpg",
+    url: "/band_images/band_photo_8.jpg",
   },
   {
     author: "Photo by Wendy Wei",
-    url: "/band_photo_9.jpg",
+    url: "/band_images/band_photo_9.jpg",
   },
 ];
