f640e3d2f12d72738223f748868fe7e51ca7b473
ojela028xamk <jerelans91@gmail.com>
2024-11-29 13:20:05 +0200

    photo order change

--- src/app/photos/photosList.ts
+++ src/app/photos/photosList.ts
@@ -6,23 +6,23 @@ type Photo = {
 export const bandPhotos: Photo[] = [
   {
     author: "Photo by Microsoft Designer",
-    url: "/band_images/band_photo_1.jpg",
+    url: "/band_images/band_photo_1.jpeg",
   },
   {
     author: "Photo by Microsoft Designer",
-    url: "/band_images/band_photo_2.jpg",
+    url: "/band_images/band_photo_2.jpeg",
   },
   {
     author: "Photo by Microsoft Designer",
-    url: "/band_images/band_photo_3.jpeg",
+    url: "/band_images/band_photo_3.jpg",
   },
   {
     author: "Photo by Microsoft Designer",
-    url: "/band_images/band_photo_4.jpeg",
+    url: "/band_images/band_photo_4.jpg",
   },
   {
     author: "Photo by Microsoft Designer",
-    url: "/band_images/band_photo_5.jpeg",
+    url: "/band_images/band_photo_5.jpg",
   },
   {
     author: "Photo by David Bartus",
@@ -34,10 +34,10 @@ export const bandPhotos: Photo[] = [
   },
   {
     author: "Photo by Vishnu R Nair",
-    url: "/band_images/band_photo_8.jpg",
+    url: "/band_images/band_photo_8.jpeg",
   },
   {
     author: "Photo by Wendy Wei",
-    url: "/band_images/band_photo_9.jpg",
+    url: "/band_images/band_photo_9.jpeg",
   },
 ];
