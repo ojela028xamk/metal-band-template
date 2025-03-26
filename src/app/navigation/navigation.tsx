3901290eca759409f0dacb0cc12dd18a5c1e8866
ojela028xamk <jerelans91@gmail.com>
2025-03-26 11:54:59 +0200

    mobile navigation and material ui

--- src/app/navigation/navigation.tsx
+++ src/app/navigation/navigation.tsx
@@ -2,13 +2,30 @@
 import Image from "next/image";
 import css from "./navigation.module.scss";
 import NavigationCard from "./navigationCard";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Link from "next/link";
 import { navigationList } from "./navigationList";
 import { usePathname } from "next/navigation";
+import {
+  Box,
+  Button,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import AccessibilityIcon from "@mui/icons-material/Accessibility";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Navigation = () => {
   const currentPath = usePathname();
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState<boolean>(false);
+
+  const toggleDrawer = (open: boolean) => {
+    setIsMobileNavOpen(open);
+  };
 
   return (
     <div
@@ -31,6 +48,34 @@ const Navigation = () => {
             <NavigationCard navItem={navItem} />
           </Fragment>
         ))}
+      </nav>
+      <nav className={css.navigation_bar_mobile}>
+        <Button className={css.mobile_menu} onClick={() => toggleDrawer(true)}>
+          <MenuIcon className={css.mobile_menu_icon} />
+        </Button>
+        <Drawer
+          anchor="bottom"
+          open={isMobileNavOpen}
+          onClose={() => toggleDrawer(false)}
+        >
+          <Box
+            onClick={() => toggleDrawer(false)}
+            onKeyDown={() => toggleDrawer(false)}
+          >
+            <List>
+              {navigationList.map((navItem) => (
+                <ListItem key={navItem.link}>
+                  <ListItemButton>
+                    <ListItemIcon>
+                      <AccessibilityIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={navItem.link} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </Drawer>
       </nav>
     </div>
   );
