cd8607d106a435e354263b8096792035c2d7a07e
ojela028xamk <jerelans91@gmail.com>
2025-03-31 17:25:01 +0300

    photos modal bug fix

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -1,42 +1,43 @@
-"use client";
+import { bandPhotos } from "./photosList";
+import BackgroundImage from "../common/backgroundImage";
+import { Dialog } from "radix-ui";
 import Image from "next/image";
 import css from "./photos.module.scss";
-import { BandPhoto, bandPhotos } from "./photosList";
-import PhotoModal from "./photoModal";
-import { useToggle } from "react-use";
-import { useState } from "react";
-import BackgroundImage from "../common/backgroundImage";
 
 const Photos = () => {
-  const [modal, toggleModal] = useToggle(false);
-  const [modalPhoto, setModalPhoto] = useState<BandPhoto>();
-
-  const handleModal = (photo: BandPhoto) => {
-    setModalPhoto(photo);
-    toggleModal();
-  };
-
   return (
     <div className={css.photos}>
-      {modal && modalPhoto && (
-        <PhotoModal toggleModal={toggleModal} modalPhoto={modalPhoto} />
-      )}
       <BackgroundImage
         source={"/background_images/background_image_photos.jpg"}
         width={2640}
         height={1760}
       />
       <div className={css.photos_grid}>
-        {bandPhotos.map((photo, index) => (
-          <Image
-            key={index}
-            className={css.band_photo}
-            src={photo.src}
-            alt={"Band photo"}
-            width={photo.width}
-            height={photo.height}
-            onClick={() => handleModal(photo)}
-          />
+        {bandPhotos.map((photo) => (
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <Image
+                className={css.band_photo}
+                src={photo.src}
+                alt={"Band photo"}
+                width={photo.width}
+                height={photo.height}
+              />
+            </Dialog.Trigger>
+            <Dialog.Portal>
+              <Dialog.Overlay className={css.modal_overlay}>
+                <Dialog.Content className={css.modal_content}>
+                  <Image
+                    className={css.modal_image}
+                    src={photo}
+                    alt={"Band photo"}
+                    width={photo.width}
+                    height={photo.height}
+                  />
+                </Dialog.Content>
+              </Dialog.Overlay>
+            </Dialog.Portal>
+          </Dialog.Root>
         ))}
       </div>
     </div>
