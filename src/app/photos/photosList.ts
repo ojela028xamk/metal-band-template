df98470ddee87f75e6f2c81d90bfca00a270a3d0
ojela028xamk <jerelans91@gmail.com>
2025-06-09 17:16:37 +0300

    photos tab optimize

--- src/app/photos/photosList.ts
+++ src/app/photos/photosList.ts
@@ -7,32 +7,32 @@ export type BandPhoto = {
 export const bandPhotos: BandPhoto[] = [
   {
     src: "/band_images/band_photo_1.jpg",
-    width: 1280,
-    height: 853,
+    width: 900,
+    height: 600,
   },
   {
     src: "/band_images/band_photo_2.jpg",
-    width: 4160,
-    height: 6240,
+    width: 900,
+    height: 1350,
   },
   {
     src: "/band_images/band_photo_3.jpg",
-    width: 4928,
-    height: 3265,
+    width: 900,
+    height: 596,
   },
   {
     src: "/band_images/band_photo_4.jpg",
-    width: 4016,
-    height: 6016,
+    width: 900,
+    height: 1348,
   },
   {
     src: "/band_images/band_photo_5.jpg",
-    width: 6720,
-    height: 4480,
+    width: 900,
+    height: 600,
   },
   {
     src: "/band_images/band_photo_6.jpg",
-    width: 3397,
-    height: 5095,
+    width: 900,
+    height: 1350,
   },
 ];
